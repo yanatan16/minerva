906345594d37040bc1bdd7f50d4caf0ef5649a5f
Jon Eisen <jon.m.eisen@gmail.com>
2012-01-17 21:12:17 -0500

    Lost portfolio test?

--- pysandbox/minerva_test/data_test/segmenter_test.py
+++ pysandbox/minerva_test/data_test/segmenter_test.py
@@ -5,15 +5,16 @@ Created on Jan 17, 2012
 '''
 import unittest
 from minerva.data import timeSeriesSegmenter
+from numpy import np
 
 
 class TimeSeriesSegmenterTestCase(unittest.TestCase):
 
 
-    def testName(self):
-        pass
+    def testBasicSegmentation(self):
+        data = np.
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    import sys;sys.argv = ['', 'PyMinerva.TimeSeriesSegmenter']
     unittest.main()
