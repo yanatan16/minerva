0486b769c8168a675d7fba25f90ae05c6759bee1
Jon Eisen <jon.m.eisen@gmail.com>
2012-04-02 23:40:13 -0400

    little cleanup.

--- pysandbox/experiments/data/get_stock_data.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pushd /home/jon/Code/Minerva/pysandbox/experiments/data
-./getStockData.py -o nasdaq_full_1990_to_2010_2 -s csv -b 19900101 -e 20101231 -i symbols/nasdaq.csv 
-popd
